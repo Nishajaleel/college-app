d1d95bab92d34f965ff18147a107a82d1f881dc7
Nishajaleel <nishanajaleel690@gmail.com>
2022-07-11 18:29:12 +0530

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentComponent } from './department/department.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentComponent
+    DepartmentComponent,
+    FacultyComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
